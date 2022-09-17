b920971d11f60027c07a3bf7f4afde16576a9afc
Quentin Eude <quentin.eude@getaround.com>
2022-09-17 17:52:39 +0200

    🎉 Init

---
+++ src/app.ts
@@ -0,0 +1,114 @@
+import "./utils/env";
+import { App, LogLevel } from "@slack/bolt";
+
+const app = new App({
+  token: process.env.SLACK_BOT_TOKEN,
+  signingSecret: process.env.SLACK_SIGNING_SECRET,
+  socketMode: true,
+  appToken: process.env.SLACK_APP_TOKEN,
+  logLevel: LogLevel.DEBUG,
+  port: 3000,
+});
+
+// let selectedRegion: string = "44";
+// let vaccineCenters: VaccineCenter[] = [];
+
+// const getChannels = async (app: App): Promise<string[]> => {
+//   const channels = await app.client.conversations.list({
+//     token: process.env.SLACK_BOT_TOKEN,
+//     exclude_archived: true,
+//   });
+//   return (channels.channels ?? [])
+//     .filter((elt) => elt.is_member && elt.is_channel && elt.name !== undefined)
+//     .map((elt) => elt.name ?? "");
+// };
+
+// const sendMessage = async (app: App, centers: VaccineCenter[]) => {
+//   const channels = await getChannels(app);
+//   channels.forEach((channel) => {
+//     app.client.chat.postMessage({
+//       token: process.env.SLACK_BOT_TOKEN,
+//       channel: channel,
+//       text: "De nouveaux créneaux sont disponibles !",
+//       blocks: buildBlocksForMessage(centers),
+//     });
+//   });
+// };
+
+// const getVaccinesCentersToNotify = (
+//   centers: VaccineCenter[]
+// ): VaccineCenter[] => {
+//   return centers.filter((center) => {
+//     const oldCenter = vaccineCenters.find((element) => element.id == center.id);
+//     console.log(`oldCenter: ${oldCenter}`);
+//     console.log(`center: ${center}`);
+//     console.log(`oldCenter chronodoses: ${oldCenter?.available_chronodoses}`);
+//     console.log(`center chronodoses: ${center.available_chronodoses}`);
+//     if (oldCenter === undefined || oldCenter === null) {
+//       return false;
+//     } else {
+//       return center.available_chronodoses > oldCenter.available_chronodoses;
+//     }
+//   });
+// };
+
+// app.command("/set_region", async ({ command, ack, client }) => {
+//   try {
+//     // Acknowledge shortcut request
+//     await ack();
+//     const test = await buildSetRegionModal(command, selectedRegion);
+//     const result = await client.views.open(JSON.parse(JSON.stringify(test)));
+//   } catch (error) {
+//     console.error(error);
+//   }
+// });
+
+// app.view("set_region_view", async ({ ack, body, view, client }) => {
+//   await ack();
+//   const value =
+//     view["state"]["values"]["set-region-input"]["set-region-action"][
+//       "selected_option"
+//     ]["value"];
+//   selectedRegion = value;
+//   vaccineCenters = [];
+//   fetch(selectedRegion).then((centers) => {
+//     vaccineCenters = centers;
+//   });
+// });
+app.message("hello", async ({ message, say }) => {
+  // Filter out message events with subtypes (see https://api.slack.com/events/message)
+  if (message.subtype === undefined || message.subtype === "bot_message") {
+    // say() sends a message to the channel where the event was triggered
+    await say({
+      blocks: [
+        {
+          type: "section",
+          text: {
+            type: "mrkdwn",
+            text: `Hey there <@${message.user}>!`,
+          },
+          accessory: {
+            type: "button",
+            text: {
+              type: "plain_text",
+              text: "Click Me",
+            },
+            action_id: "button_click",
+          },
+        },
+      ],
+      text: `Hey there <@${message.user}>!`,
+    });
+  }
+});
+// app.command("/region", async ({ command, ack, say }) => {
+//   await ack();
+//   await say(`La région sélectionnée est: *${selectedRegion}*`);
+// });
+
+(async () => {
+  // Start your app
+  await app.start();
+  console.log(process.env.SLACK_BOT_TOKEN);
+  console.log("⚡️ Bolt app is running!");
+})();
